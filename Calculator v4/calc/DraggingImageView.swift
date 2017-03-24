62ac4047f33fbfe8b0a7eb8ad889a2ba7c37bb7d
Prithpal <sunnylall@hotmail.co.uk>
2017-03-24 14:51:03 +0000

    Add files via upload

---
+++ Calculator v4/calc/DraggingImageView.swift
@@ -0,0 +1,41 @@
+//
+//  DraggingImageView.swift
+//  calc
+//
+//  Created by pl14aaq on 21/03/2017.
+//  Copyright © 2017 pl14aaq. All rights reserved.
+//
+
+import UIKit
+
+class DraggingImageView: UIImageView {
+
+    var startlocation: CGPoint?
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        startlocation = touches.first?.locationInView(self)
+    }
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        let currentLocation = touches.first?.locationInView(self)
+        let dx = currentLocation!.x - startlocation!.x
+        let dy = currentLocation!.y - startlocation!.y
+        
+        self.center = CGPointMake(self.center.x+dx, self.center.y+dy)
+    }
+    
+    func shake () {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 2.05
+        animation.repeatCount = 3
+        animation.autoreverses = true
+        animation.fromValue = NSValue(CGPoint: CGPoint(x: self.center.x, y: self.center.y - 4))
+        animation.toValue = NSValue(CGPoint: CGPoint(x: self.center.x, y: self.center.y + 4))
+        
+        self.layer.addAnimation(animation, forKey: "position")
+    }
+    
+    
+}
